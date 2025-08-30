53e7eb0366196d0b378a92d31d3b55debe00cbde
RokasAl <rokas@incentivegames.com>
2025-08-30 20:25:45 +0100

    added mqtt publishing

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+import { IoTDataPlaneClient, PublishCommand } from "@aws-sdk/client-iot-data-plane";
+import 'dotenv/config';
+
+const client = new IoTDataPlaneClient({ region: process.env.AWS_REGION });
+
 exports.handler = async (event) => {
-  console.log("Event received:", event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
-  };
+
+  if(event?.topic === process.env.AWS_IOT_TOPIC){
+      const command = new PublishCommand({
+      topic: process.env.AWS_IOT_TOPIC,
+      qos: 1,
+      payload: Buffer.from(JSON.stringify({ message: {data:{motorState:event.motorState}}}))
+    });
+
+    await client.send(command);
+    console.log("Message published!");
+  }
+
 };
