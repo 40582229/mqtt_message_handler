7d48b0151f813615b088971a51df5e90822571e0
RokasAl <rokas@incentivegames.com>
2025-08-31 13:51:02 +0100

    Adde object freeze

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { IoTDataPlaneClient, PublishCommand } = require("@aws-sdk/client-iot-data-plane");
-require("dotenv").config(); // loads .env when running locally
+const { TOPICS } = require("./src/topics");
+require("dotenv").config();
 
 const client = new IoTDataPlaneClient({ region: process.env.NODE_AWS_REGION });
 
 exports.handler = async (event) => {
 
-  if(event?.topic === process.env.NODE_AWS_IOT_TOPIC){
+  if(event?.topic === TOPICS.AWS_TO_ESP32){
       const command = new PublishCommand({
       topic: process.env.NODE_AWS_IOT_TOPIC,
       qos: 1,
       payload: Buffer.from(JSON.stringify({ message: {data:{motorState:event.motorState}}}))
     });
-
     await client.send(command);
     console.log("Message published!");
+  } else if(event?.topic === TOPICS.ESP32_TO_AWS){
+    console.log(JSON.parse(event));
+    console.log("Message received!");
   }
 
 };
