5aa61cc87d0ed1771415dd0b066c9c22a2fdfed4
RokasAl <rokas@incentivegames.com>
2025-09-07 21:22:02 +0100

    added ability to send mqtt messages from webscokets

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const client = new IoTDataPlaneClient({ region: process.env.NODE_AWS_REGION });
 
 exports.handler = async (event) => {
   console.log("EVENT RECEIVED:", event);
-  const { eventType, connectionId, domainName, stage } = event?.requestContext ?? {};
+  const { eventType, connectionId, body, domainName, stage } =
+    event?.requestContext ?? {};
   if (eventType && eventType === "CONNECT") {
     const command = new PublishCommand({
       topic: TOPICS.AWS_TO_ESP32,
@@ -36,17 +37,23 @@ exports.handler = async (event) => {
     await client.send(command);
     console.log("Message published!");
   }
-  if (event?.topic === TOPICS.AWS_TO_ESP32) {
-    const command = new PublishCommand({
-      topic: process.env.NODE_AWS_IOT_TOPIC,
-      qos: 1,
-      payload: Buffer.from(
-        JSON.stringify({ message: { data: { motorState: event.motorState } } })
-      ),
-    });
-    await client.send(command);
-    console.log("Message published!");
-  } else if (event?.topic === TOPICS.ESP32_TO_AWS) {
+  if (eventType && eventType === "MESSAGE") {
+    if (body?.topic === TOPICS.AWS_TO_ESP32) {
+      const command = new PublishCommand({
+        topic: process.env.TOPICS.AWS_TO_ESP32,
+        qos: 1,
+        payload: Buffer.from(
+          JSON.stringify({
+            message: { data: { motorState: body.motorState } },
+          })
+        ),
+      });
+      await client.send(command);
+      console.log("Message published!");
+    }
+  }
+
+  if (event?.topic === TOPICS.ESP32_TO_AWS) {
     console.log(event);
     console.log("Message received!");
     const client = new ApiGatewayManagementApiClient({
@@ -57,7 +64,7 @@ exports.handler = async (event) => {
     try {
       await client.send(
         new PostToConnectionCommand({
-          ConnectionId: event.data.connectionId, // 🔑 must include the exact connectionId
+          ConnectionId: event.data.connectionId,
           Data: Buffer.from(JSON.stringify(event)),
         })
       );
@@ -70,6 +77,6 @@ exports.handler = async (event) => {
       }
     }
   }
-  // ''
+
   return { statusCode: 200 };
 };
